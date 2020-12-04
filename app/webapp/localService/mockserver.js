c8c9d2385e61248a2b833d816b851fb2cf54abd9
georgina95 <georgina95@gmail.com>
2020-12-04 14:31:16 +0000

    delete mock server

--- app/webapp/localService/mockserver.js
+++ app/webapp/localService/mockserver.js
@@ -7,7 +7,7 @@ sap.ui.define([
     "use strict";
 
     var oMockServer,
-        _sAppPath = "elte_grafmodellezo.app/",
+        _sAppPath = "eltegrafmodellezo/app/",
         _sJsonFilesPath = _sAppPath + "localService/mockdata";
 
     var oMockServerInterface = {
